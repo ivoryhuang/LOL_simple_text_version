3332e82493409aeef380cc1fd180c47eab69d081
ivoryhuang <ivorybangle99@gmail.com>
2017-01-19 17:47:39 +0800

    update test.py

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 #-*- coding: utf-8 -*-
-from games import ARAM
+from games import ARAM, Item_Shop
 from items import *
 from champs import Brand, Ziggs
 
 aram = ARAM()
+item_shop = Item_Shop()
 
 #member_idx: assume每隊5人, team1 0~4, team2 5~9
 
-Brand = Brand(aram_center=aram, team_idx=0, member_idx=0)
-Ziggs = Ziggs(aram_center=aram, team_idx=1, member_idx=1)
+Brand = Brand(aram_center=aram, team_idx=0, member_idx=0, item_shop=item_shop)
+Ziggs = Ziggs(aram_center=aram, team_idx=1, member_idx=1, item_shop=item_shop)
 aram.compose_team([Brand, Ziggs])
-
+'''
 Brand.move((10, 10))
 Ziggs.move((20, 20))
 Brand.lvup()
@@ -23,21 +24,30 @@ Ziggs.learn_q()
 Ziggs.learn_w()
 Ziggs.learn_e()
 Ziggs.learn_r()
-
+'''
 Lost_Chapter = Lost_Chapter()
 Boots_Of_Speed = Boots_Of_Speed()
 Morellonomicon = Morellonomicon()
 Amplifying_Tome = Amplifying_Tome()
-Morellonomicon.get_data()
+Sapphire_Crystal = Sapphire_Crystal()
+Sorcerers_Shoes = Sorcerers_Shoes()
+#Morellonomicon.get_data()
 
-Ziggs.buy_item(Lost_Chapter)
-Ziggs.buy_item(Boots_Of_Speed)
-Ziggs.sell_item(Boots_Of_Speed)
+print(Ziggs.buy_item(1001))
+print(Ziggs.buy_item(1052))
+print(Ziggs.buy_item(3802))
+print(Ziggs.buy_item(3020))
+#print(Ziggs.items)
+print(item_shop.display_items_to_sell())
+print(item_shop.display_items_to_recycle(Ziggs))
+#print(Ziggs.sell_item(1))
+#Ziggs.buy_item(Boots_Of_Speed)
+#Ziggs.sell_item(Boots_Of_Speed)
 
 #Ziggs.buy_item(Morellonomicon)
 #Ziggs.sell_item(Morellonomicon)
 
-
+'''
 
 Brand.trigger_q((0, 0))
 
@@ -47,4 +57,4 @@ Ziggs.trigger_e((10, 10))
 Ziggs.trigger_r((10, 10))
 Ziggs.trigger_r((10, 10))
 Ziggs.trigger_r((10, 10))
-
+'''
